b13f527fb77de848dddf769f5d6f50354359ffa9
zjng <78717633+zjng@users.noreply.github.com>
2021-12-15 12:42:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/linkease/istore.git package/lean/istore
+#git clone https://github.com/linkease/istore.git package/lean/istore
